fff0b2a9007925b4b5363a64a690370671619a0e
Luiz Augusto Tavares da Silveira <latsnet@gmail.com>
2022-11-29 23:51:14 -0300

    New Category Model

---
+++ Models/Category.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mvc_project.Models;
+
+[Table("Categories")]
+public class Category
+{
+    [Key]
+    public int CategoryId { get; set; }
+
+    [StringLength(100, ErrorMessage = "Max length is 100 characters")]
+    [Required(ErrorMessage = "Please inform the Category Name")]
+    [Display(Name = "Name")]
+    public string CategoryName { get; set; }
+
+    [StringLength(200, ErrorMessage = "Max length is 200 characters")]
+    [Required(ErrorMessage = "Please inform the description")]
+    [Display(Name = "Description")]
+    public string Description { get; set; }
+    
+}
